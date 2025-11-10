a73ae8c2f55d7f51f6111a2b42dad0057330de5c
Khoahd <khoahd@uht.com.vn>
2025-11-10 14:12:33 +0700

    fix: keep config outside src and include in package

--- src/Providers/MqBridgeServiceProvider.php
+++ src/Providers/MqBridgeServiceProvider.php
@@ -9,14 +9,14 @@ class MqBridgeServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->publishes([
-            __DIR__ . '/../config/mq_bridge.php' => config_path('mq_bridge.php'),
+            __DIR__ . '/../../config/mq_bridge.php' => config_path('mq_bridge.php'),
         ], 'mq-bridge');
     }
 
     public function register(): void
     {
         $this->mergeConfigFrom(
-            __DIR__.'/../config/mq_bridge.php',
+            __DIR__.'/../../config/mq_bridge.php',
             'mq_bridge'
         );
     }
